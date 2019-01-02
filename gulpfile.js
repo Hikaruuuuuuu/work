dc8639b0eca37ebd2fbd8ac4eb70c4a87c3cb90a
Hikaru <229595976@qq.com>
2019-01-02 09:05:42 +0800

    搭建自动化开发环境

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    clean = require('gulp-clean-css'),
+    uglify = require('gulp-uglify'),
+    server = require('gulp-webserver'),
+    fs = require('fs'),
+    path = require('path'),
+    url = require('url');
