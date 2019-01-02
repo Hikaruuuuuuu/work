6556d3f15bb2a99e3d8acdd2b45bb0a84c739660
Hikaru <229595976@qq.com>
2019-01-02 09:14:40 +0800

    启动web服务器

--- gulpfile.js
+++ gulpfile.js
@@ -8,3 +8,31 @@ var gulp = require('gulp'),
     fs = require('fs'),
     path = require('path'),
     url = require('url');
+
+// 启动web服务 src
+gulp.task('serversrc', function(){
+    return serverRun('src')
+})
+
+// 启动web服务 dist
+gulp.task('serverdist', function(){
+    return serverRun('dist')
+})
+
+function serverRun(serverPath){
+    return gulp.src(serverPath)
+        .pipe(server({
+            port: 8000,
+            open: true,
+            livereload: true,  //自动刷新
+            middleware: function(req, res, next){
+                var pathname = url.parse(req.url).pathname;
+                if(pathname == '/favicon.ico'){
+                    return res.end()
+                }
+
+                pathname = pathname == '/' ? 'index.html' : pathname;
+                res.end(fs.readFileSync(path.join(__dirname, serverPath, pathname)))
+            }
+        }))
+}
